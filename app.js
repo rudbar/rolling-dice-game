c13b9604ba12aa58e6abba448bb98c83862febd3
Jan Kachan <31417194+rudbar@users.noreply.github.com>
2020-01-19 20:04:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,70 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-1').textContent;
-console.log(x);
-
+//make the rolling dice invisible at the beginning of the game
 document.querySelector('.dice').style.display = 'none';
+
+//set all the scores to 0
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. Generate a random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = "dice-" + dice + ".png";
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dice = Math.floor(Math.random() * 6) + 1;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//var x = document.querySelector('#score-1').textContent;
+//console.log(x);
+
